654dc4e38f32fd5a003bdfc762798a87a7c9b04e
Erikcc <marccandia9@gmail.com>
2022-12-21 22:02:45 +0100

    3 test realizados

--- src/films.js
+++ src/films.js
@@ -1,18 +1,27 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  let result =  array.map(array => array.director);
+  //console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let newArray = array.filter(pelicula => pelicula.director == "Francis Ford Coppola");
+  //console.log("EXERCICE 2 ->", newArray);
+  return newArray;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  let directores = array.filter(pelicula => pelicula.director == "Christopher Nolan")//filtro por nombre del director
+  let promedio = directores.reduce((total, actual) => total + actual.score, 0); //sumo el score de directores
+  //let posicion = directores.findIndex(index => index.score == 8.5) //para ver la posicion dnd se encuentra ese objeto
+  let totalIndices = directores.filter(title => title.director = "Christopher Nolan").length;//da el total de objetos 4
+  let average = promedio/totalIndices;
+  //let average2 = (promedio/totalIndices).toFixed(2);//redondeado a 2 decimales
+  console.log(average);
+  return average;
 }
 
 // Exercise 4:  Alphabetic order by title 

5564af4a2b2676a9908a5fe354bcce2d96c1ed11
Erikcc <marccandia9@gmail.com>
2023-01-09 18:39:59 +0100

    Segunda version

--- src/films.js
+++ src/films.js
@@ -6,32 +6,42 @@ function getAllDirectors(array) {
 }
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {
-  let newArray = array.filter(pelicula => pelicula.director == "Francis Ford Coppola");
-  //console.log("EXERCICE 2 ->", newArray);
+function getMoviesFromDirector(array, director = "Francis Ford Coppola") {
+  let newArray = array.filter(pelicula => pelicula.director == director);//filtra por director
+  let arrayDirector =  newArray.map(director => director.title);//recogemos las peliculas de ese director
+  //console.log("EXERCICE 2 ->", arrayDirector);
   return newArray;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {
-  let directores = array.filter(pelicula => pelicula.director == "Christopher Nolan")//filtro por nombre del director
+function moviesAverageOfDirector(array, director = "Christopher Nolan") {
+  let directores = array.filter(pelicula => pelicula.director == director)//filtro por nombre del director
   let promedio = directores.reduce((total, actual) => total + actual.score, 0); //sumo el score de directores
   //let posicion = directores.findIndex(index => index.score == 8.5) //para ver la posicion dnd se encuentra ese objeto
-  let totalIndices = directores.filter(title => title.director = "Christopher Nolan").length;//da el total de objetos 4
-  let average = promedio/totalIndices;
-  //let average2 = (promedio/totalIndices).toFixed(2);//redondeado a 2 decimales
-  console.log(average);
+  //let totalIndices = directores.filter(title => title.director = director).length;//da el total de objetos 8
+  let peliDirector =  directores.map(director => director.title).length; //filtra las peliculas de ese Director
+  let average = promedio/peliDirector;
+  //console.log(average);
   return average;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
+  let peliculas = array.sort(); //ordenado el array
+  let ordenar = array.sort((x,y) => x.title.localeCompare(y.title));//Ordena por titulo
+  let filtrarTitle = ordenar.map(titulo => titulo.title); //montamos array para que muestre solo titulos como array de strings
+  
+  //console.log(filtrarTitle);
+  return filtrarTitle;
   
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(array) {
+  let orderYear= array.sort((x,y) => x.year - y.year);//Ordena por año el array de objetos
+  newArray = [];
+  orderYear = newArray;
+  return newArray;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
